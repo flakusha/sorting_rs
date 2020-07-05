c7b071e2e3fa4ac5d778ba6c6889a9c2dfdb1551
flakusha <zenflak@gmail.com>
2020-07-05 11:44:16 +0300

    Removed spaces from documentation and readme for better readability

--- README.md
+++ README.md
@@ -7,9 +7,9 @@
 | Cocktail | little performance improvement over bubble sort | `n`<sup>`2`</sup> | `n` | `1` |
 | Comb | speeds up when data is nearly sorted | `n`<sup>`2`</sup> | `n log n` | `1` |
 | Gnome | simple and slow, works with one item at a time | `n`<sup>`2`</sup> | `n` | `1` |
-| Heap | independent of data distribution | `n log n` | `n log n` or `n` | `n` or `1` |
+| Heap | independent of data distribution | `nlogn` | `nlogn` or `n` | `n` or `1` |
 | Insertion | simple, but less effective than quicksort, heapsort or merge sort | `n`<sup>`2`</sup>; `n`<sup>`2`</sup> | `n`; `1` | `n` or `1` |
-| Merge | independent of data distribution | `n log n` | `n log n` or `n` | `n` |
+| Merge | independent of data distribution | `nlogn` | `nlogn` or `n` | `n` |
 | Quick | bad for sorted or reversed input | `n`<sup>`2`</sup> | `n` | `n` or `log n` |
 | Selection | the least number of swaps among all the algorithms | `n`<sup>`2`</sup>; `n` | `n`<sup>`2`</sup>; `1` | `1` |
 | Shell | it is optimization of insertion sort | `n`<sup>`2`</sup> or `n log`<sup>`2`</sup> `n` | `n log n` or `n log`<sup>`2`</sup> `n` | `n` |
--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,14 @@
 //! | -------------- | -------------------------------- | -------- | -------- | ------------- |
 //! | Bubble | bad for sorted or reversed input | `n`<sup>`2`</sup>; `n`<sup>`2`</sup> | `n`; `1` | `n` or `1` |
 //! | Cocktail | little performance improvement over bubble sort | `n`<sup>`2`</sup> | `n` | `1` |
-//! | Comb | speeds up when data is nearly sorted | `n`<sup>`2`</sup> | `n log n` | `1` |
+//! | Comb | speeds up when data is nearly sorted | `n`<sup>`2`</sup> | `nlogn` | `1` |
 //! | Gnome | simple and slow, works with one item at a time | `n`<sup>`2`</sup> | `n` | `1` |
-//! | Heap | independent of data distribution | `n log n` | `n log n` or `n` | `n` or `1` |
+//! | Heap | independent of data distribution | `nlogn` | `n log n` or `n` | `n` or `1` |
 //! | Insertion | simple, but less effective than quicksort, heapsort or merge sort | `n`<sup>`2`</sup>; `n`<sup>`2`</sup> | `n`; `1` | `n` or `1` |
-//! | Merge | independent of data distribution | `n log n` | `n log n` or `n` | `n` |
-//! | Quick | bad for sorted or reversed input | `n`<sup>`2`</sup> | `n` | `n` or `log n` |
+//! | Merge | independent of data distribution | `nlogn` | `nlogn` or `n` | `n` |
+//! | Quick | bad for sorted or reversed input | `n`<sup>`2`</sup> | `n` | `n` or `logn` |
 //! | Selection | the least number of swaps among all the algorithms | `n`<sup>`2`</sup>; `n` | `n`<sup>`2`</sup>; `1` | `1` |
-//! | Shell | it is optimization of insertion sort | `n`<sup>`2`</sup> or `n log`<sup>`2`</sup> `n` | `n log n` or `n log`<sup>`2`</sup> `n` | `n` |
+//! | Shell | it is optimization of insertion sort | `n`<sup>`2`</sup> or `nlog`<sup>`2`</sup> `n` | `nlogn` or `nlog`<sup>`2`</sup>`n` | `n` |
 
 pub mod bubble_sort;
 pub mod cocktail_sort;
